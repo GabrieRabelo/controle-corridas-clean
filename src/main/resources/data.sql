214af561594eaca2ad575eaa89f86f36661e8910
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2021-04-19 11:52:35 -0300

    adiciona data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO corredor (cpf, nome, dia_dn, mes_dn, ano_dn, genero) values ('12312312312', 'Rabelo', 1, 12, 1997, 'M')
-INSERT INTO corredor (cpf, nome, dia_dn, mes_dn, ano_dn, genero) values ('12312312311', 'Vinicius', 1, 12, 1996, 'M')
+INSERT INTO corredor (cpf, nome, dia_dn, mes_dn, ano_dn, genero) values ('12312312312', 'Rabelo', 1, 12, 1997, 'M');
+INSERT INTO corredor (cpf, nome, dia_dn, mes_dn, ano_dn, genero) values ('12312312311', 'Vinicius', 1, 12, 1996, 'M');
